86d576f06f17da555f1747e5d48642b820eb56ae
Anay_Gupta <anayg.hestabit@gmail.com>
2026-03-18 19:19:31 +0530

    Emails Resolved

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,14 +1,28 @@
 const nodemailer = require('nodemailer');
+const dns = require('dns');
 const logger = require('./logger');
+
+const resolveSmtpHost = async (host) => {
+  const lookup = await dns.promises.lookup(host, { family: 4 });
+  return lookup.address;
+};
 
 const sendEmail = async (options) => {
   try {
+    const smtpHost = process.env.SMTP_HOST;
+    const resolvedHost = await resolveSmtpHost(smtpHost);
+    const port = Number(process.env.SMTP_PORT);
+
     const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
+      host: resolvedHost,
+      port,
+      secure: port === 465,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
+      },
+      tls: {
+        servername: smtpHost,
       },
     });
 
